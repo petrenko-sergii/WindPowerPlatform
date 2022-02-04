d9ca09676c4b6a3c7a9d881a14643397c09c0ff9
Petrenko Sergii <petrenko7@yahoo.com>
2022-02-04 18:01:28 +0200

    Add Dockerfile to CommandsService

--- WindPowerPlatformService/Startup.cs
+++ WindPowerPlatformService/Startup.cs
@@ -57,7 +57,7 @@ namespace WindPowerPlatformService
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WindPowerPlatformService v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
